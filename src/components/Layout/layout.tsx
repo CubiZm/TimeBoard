7e7bf512c5d50dda33d4df4a4a871bb98e117eca
CubiZm <karelense@gmail.com>
2020-05-19 23:07:53 +0300

    Updates

--- src/components/Layout/layout.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { PageProps } from "gatsby"
-
-const Layout = (props: PageProps) => {
-  return <div className="main-wrapper">{props.children}</div>
-}
-
-export default Layout
